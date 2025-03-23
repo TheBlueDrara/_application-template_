2943a68915546feefea66548b14cfea0ffd92910
alex <alex@gmail.com>
2025-03-23 19:45:09 +0200

    initial commit

---
+++ tools.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash 
+###################################### Start Safe Header ######################################################
+#Developed by Alex Umansky aka TheBlueDrara
+#Porpuse: to check if neccery tools are installed, and if not install them
+#Date: 21.03.2025
+set -o nounset
+set -o exiterr
+set -o pipefail
+###################################### End Safe header #######################################################
+source tool_list.txt
+
+
+if [[ $ID_LIKE == "*debian*" ]]; then
+	echo "Running on Debian-family distro. Executing main code..."
+else
+	echo "This script is designed to run only on Debian-family distory only!"
+	exit 1
+fi
+
+function main(){
+	for index in TOOLS; do
+		check_and_install "$index"
+	done
+}
+
+function check_and_install(){
+	package=$1
+	if ! dpkg -l | grep -q "^ii $package "; then
+		sudo apt-get install "$package" -y
+		return 1
+	else
+		return 0
+	fi
+}
+
+main
